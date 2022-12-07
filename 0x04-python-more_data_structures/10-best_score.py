cd1d1c3d59563c5568bcea3f727cb4fddaf17fca
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-12-07 08:48:59 -0800

    task 2,10,11

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if a_dictionary:
+       return max(a_dictionary, key=a_dictionary.get)
+    else:
+        return None
